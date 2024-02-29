4df4aff81439c6a55f89adf17c7becde2656b40f
mzazakeith <mzazakeith@gmail.com>
2024-02-29 23:50:03 +0300

    Initial Home shell

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,13 @@
+import Image from "next/image";
+import {useRouter} from "next/router";
 const Avatar = () => {
-  return <div>Avatar</div>;
+  const router = useRouter();
+  return (
+      <div className='hidden xl:flex xl:max-w-none'>
+        <Image src={`${router.basePath}/avatar.png`} alt='' width={737} height={678}
+               className='translate-z-0 w-full h-full'/>
+      </div>
+  );
 };
 
 export default Avatar;
