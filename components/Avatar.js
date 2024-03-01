2dabb8d73137ab753d8b0536eebc8f493601e8c2
mzazakeith <mzazakeith@gmail.com>
2024-03-02 01:13:28 +0300

    initial avatar added

--- components/Avatar.js
+++ components/Avatar.js
@@ -4,7 +4,7 @@ const Avatar = () => {
   const router = useRouter();
   return (
       <div className='hidden xl:flex xl:max-w-none'>
-        <Image src={`${router.basePath}/avatar.png`} alt='' width={737} height={678}
+        <Image src={`${router.basePath}/my-avatar.png`} alt='' width={737} height={678}
                className='translate-z-0 w-full h-full'/>
       </div>
   );
