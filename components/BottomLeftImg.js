3e75bf364718ce29a85753ac09e5fbfc5295a0e9
mzazakeith <mzazakeith@gmail.com>
2024-03-02 11:39:19 +0300

    hiding bottom images on small screens

--- components/BottomLeftImg.js
+++ components/BottomLeftImg.js
@@ -5,7 +5,7 @@ const BottomLeftImg = () => {
     const router = useRouter();
     const pathname = router.pathname
   return (
-      <div className='absolute left-1 bottom-0 z-10 w-[1000px] xl:w-[150px] -rotate-25 animate-pulse'>
+      <div className='hidden xl:absolute left-1 bottom-0 z-10 xl:w-[150px] -rotate-25 animate-pulse'>
         <Image src={`${router.basePath}/terminal-icon.png`} width={200} height={200} alt=''/>
       </div>
   );
--- components/BottomRightImg.js
+++ components/BottomRightImg.js
@@ -5,7 +5,7 @@ const BottomRightImg = () => {
     const router = useRouter();
 
     return (
-        <div className='absolute right-0 bottom-0 z-10 xl:w-[150px] rotate-25 opacity-80'>
+        <div className='hidden xl:absolute right-0 bottom-0 z-10 xl:w-[150px] rotate-25 opacity-80'>
             <Image src={`${router.basePath}/code-icon.png`} width={300} height={300} alt=''/>
         </div>
     );
