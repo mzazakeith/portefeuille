5bf374a82a5a932779ce8af74f2c9f38d69e3ddc
mzazakeith <mzazakeith@gmail.com>
2024-03-04 21:04:34 +0300

    Projects title and animation

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,9 +1,17 @@
+import {fadeIn} from "../../variants";
+import {motion} from "framer-motion";
+
 const Projects = () => {
   return (
       <>
-        <main>
-
-        </main>
+        <div className="h-full py-32 text-center xl:text-left">
+          <div className='container mx-auto h-full flex flex-col items-center gap-x-6'>
+            <motion.h1
+                variants={fadeIn('down', 0.2)} initial='hidden' animate='show' exit='hidden'
+                className='h1 mt-2 xl:mt-auto'> WE LEARN BY PRACTICE<span className='text-accent'>.</span>
+            </motion.h1>
+          </div>
+        </div>
       </>
   );
 };
