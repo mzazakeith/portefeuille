f0ba086a563a872293c7ad2a454e0962628043a4
mzazakeith <mzazakeith@gmail.com>
2024-03-04 21:08:41 +0300

    Layout done

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -10,6 +10,36 @@ const Projects = () => {
                 variants={fadeIn('down', 0.2)} initial='hidden' animate='show' exit='hidden'
                 className='h1 mt-2 xl:mt-auto'> WE LEARN BY PRACTICE<span className='text-accent'>.</span>
             </motion.h1>
+
+            <div className="grid grid-cols-12 gap-24">
+              <div className="col-span-12">
+                Featured Project
+              </div>
+              <div className="col-span-6">
+                Project - 1
+              </div>
+              <div className="col-span-6">
+                Project - 2
+              </div>
+              <div className="col-span-12">
+                Featured Project -2
+              </div>
+              <div className="col-span-6">
+                Project - 3
+              </div>
+              <div className="col-span-6">
+                Project - 4
+              </div>
+              <div className="col-span-12">
+                Featured Project - 3
+              </div>
+              <div className="col-span-6">
+                Project - 5
+              </div>
+              <div className="col-span-6">
+                View More Button
+              </div>
+            </div>
           </div>
         </div>
       </>
