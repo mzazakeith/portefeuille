1862f66d62b6faaa644c9cffb77d99a84ccd8f32
mzazakeith <mzazakeith@gmail.com>
2024-03-05 20:49:41 +0300

    Adding animation to bottom images

--- components/BottomLeftImg.js
+++ components/BottomLeftImg.js
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import {useRouter} from "next/router";
 
+import {motion} from 'framer-motion';
+import {fadeIn} from "../variants";
+
+
 const BottomLeftImg = () => {
     const router = useRouter();
     const pathname = router.pathname
   return (
-      <div className={`absolute hidden ${pathname === "/" && 'xl:block'} left-1 bottom-0 z-10 xl:w-[150px] -rotate-25 animate-pulse`}>
-        <Image src={`${router.basePath}/terminal-icon.png`} width={120} height={120} alt=''/>
-      </div>
+      <motion.div
+          variants={fadeIn('right', 0.4)} initial='hidden' animate='show' exit='hidden'
+          className={`absolute hidden ${pathname === "/" && 'xl:block'} left-1 bottom-0 z-10 
+          xl:w-[150px]`}>
+        <Image className="-rotate-25 animate-pulse" src={`${router.basePath}/terminal-icon.png`} width={120} height={120} alt=''/>
+      </motion.div>
   );
 };
 
