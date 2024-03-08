1717b06a859ff34b1fc7cd8728f9821fee45d20d
mzazakeith <mzazakeith@gmail.com>
2024-03-08 21:27:02 +0300

    removing fixed header nav

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Socials from "../components/Socials";
 
 const Header = () => {
   return (
-      <header className='fixed z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'>
+      <header className='z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'>
           <div className='container mx-auto'>
               <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
                   <Link className="text-[2.6rem] font-poppins"  href="">
