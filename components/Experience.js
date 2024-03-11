a911bfbd1b88c1932b43c9b251e37c82ab7c2bc3
mzazakeith <mzazakeith@gmail.com>
2024-03-11 19:55:56 +0300

    experience styling

--- components/Experience.js
+++ components/Experience.js
@@ -5,21 +5,21 @@ import LiIcon from "../components/Lilcon";
 const Details = ({position, company, companyLink, time, address, work}) =>{
     const ref = useRef(null)
     return (
-        <li ref={ref} className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-center">
+        <li ref={ref} className="mt-8 first:mt-0 w-[60%] mx-auto flex flex-col items-center justify-center">
             <LiIcon reference={ref}/>
             <motion.div
             initial={{y:50}}
             whileInView={{y:0}}
             transition={{duration:0.5, type:"spring"}}
             >
-                <h3 className="capitalize font-bold text-2xl">{position} <a
-                    className="capitalize text-accent"
+                <h3 className="uppercase font-medium text-2xl">{position} <a
+                    className="uppercase font-light text-accent/90 ml-0.5"
                     href={companyLink}
-                    target={"_blank"}>@{company}</a></h3>
-                <span className="capitalize font-medium">
+                    target={"_blank"}>@ {company}</a></h3>
+                <span className="capitalize font-light mt-4">
                     {time} | {address}
                 </span>
-                <p className="font-medium w-full">
+                <p className="mt-2 w-full">
                     {work}
                 </p>
             </motion.div>
@@ -37,12 +37,12 @@ const Experience = () => {
     )
     return (
         <div className="my-32">
-            <h2 className="font-bold text-8xl mb-32 w-full text-center">Experience</h2>
+            <h2 className="font-light text-neutral-900 h2 uppercase mb-32 w-full text-center">Experience</h2>
 
             <div ref={ref} className="w-[75%] mx-auto relative">
                 <motion.div
                     style={{scaleY:scrollYProgress}}
-                    className="absolute h-full left-9 top-0 w-[4px] bg-black origin-top"/>
+                    className="absolute h-full left-9 top-0 w-[4px] bg-neutral-900 origin-top"/>
                 <ul className="w-full flex flex-col items-start justify-between ml-4">
                     <Details
                         position='Software Developer' company='Amazon'
