842c718d869d2e8b753f9feb2ca8d4d97089c9c2
mzazakeith <mzazakeith@gmail.com>
2024-03-11 16:30:55 +0300

    more styling for texts

--- components/BusinessCard.js
+++ components/BusinessCard.js
@@ -6,38 +6,38 @@ const BusinessCard = () => {
     const router = useRouter();
     return(
         <div
-            className="relative min-h-[200px] max-w-[400px] w-full rounded-lg shadow-2xl bg-[#BEB5AF] m-[2rem] overflow-hidden">
+            className="relative min-h-[200px] max-w-[400px] w-full rounded-lg shadow-2xl bg-[#BEB5AF] m-8 mt-16 overflow-hidden">
             <div className="flex flex-col justify-center m-4 mb-0">
-                <div className="flex flex-row items-start justify-between">
-                    <h1 className="text-2xl">Keith Mzaza</h1>
-                    <div className="bg-[#BEB5AF] rounded-md z-10
-                border border-solid flex flex-col justify-center items-center border-[#A69F99]/50">
-                        <Image src={`${router.basePath}/my-avatar.png`} alt='' width={50} height={50}
+                <div className="flex flex-row items-start justify-between z-10">
+                    <h1 className="text-3xl mt-2 font-semibold">Keith Mzaza</h1>
+                    <div className="bg-[#BEB5AF] rounded-md
+                border border-solid flex flex-col justify-center items-center min-h-[55px] min-w-[55px] border-[#A69F99]/50">
+                        <Image className="mb-1" src={`${router.basePath}/my-avatar.png`} alt='' width={45} height={45}
                         />
                     </div>
                 </div>
                 <div className="flex flex-row items-start">
-                    <p className="text-accent">Fullstack Developer & DevOps Engineer</p>
+                    <p className="text-accent z-10 text-sm">Fullstack Developer & DevOps Engineer</p>
                 </div>
             </div>
 
             <div className="flex flex-col pb-5 pt-2 gap-1">
                 <div className="grid grid-cols-2 mr-8">
                     <span className="text-black font-bold col-span-1 text-left ml-4">email</span>
-                    <span className="font-light z-10 col-span-1 text-left">mzazakeith@gmail.com</span>
+                    <span className="font-light z-10 col-span-1 text-left underline">mzazakeith@gmail.com</span>
                 </div>
                 <div className="grid grid-cols-2 mr-8">
                     <span className="text-black font-bold col-span-1 text-left ml-4">linkedIn</span>
-                    <span className="font-light z-10 col-span-1 text-left">k-mzaza</span>
+                    <span className="font-light z-10 col-span-1 text-left underline">k-mzaza</span>
                 </div>
                 <div className="grid grid-cols-2 mr-8">
                     <span className="text-black font-bold col-span-1 text-left ml-4">github</span>
-                    <span className="font-light z-10 col-span-1 text-left">mzazakeith</span>
+                    <span className="font-light z-10 col-span-1 text-left underline">mzazakeith</span>
                 </div>
 
                 <div className="grid grid-cols-2 mr-8">
                     <span className="text-black font-bold col-span-1 text-left ml-4">hashnode</span>
-                    <span className="font-light z-10 col-span-1 text-left">mzaza.hashnode.dev</span>
+                    <span className="font-light z-10 col-span-1 text-left underline">mzaza.hashnode.dev</span>
                 </div>
             </div>
 
