1a312f391a808c3852ce18ccc1db764ca8ad48c2
mzazakeith <mzazakeith@gmail.com>
2024-03-11 12:13:39 +0300

    resizing and hiding overflow

--- pages/more/index.js
+++ pages/more/index.js
@@ -3,6 +3,65 @@ import {motion} from "framer-motion";
 import Link from "next/link";
 import Image from "next/image";
 
+
+const Background = ( ) => {
+  return (
+      <motion.svg
+          width="30rem"
+          height="30rem"
+          viewBox="0 0 320 384"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 z-0"
+          variants={{
+            hover: {
+              scale: 1.5,
+            },
+          }}
+          transition={{
+            duration: 1,
+            ease: "backInOut",
+          }}
+      >
+        <motion.circle
+            variants={{
+              hover: {
+                scaleY: 0.5,
+                y: -25,
+              },
+            }}
+            transition={{
+              duration: 1,
+              ease: "backInOut",
+              delay: 0.2,
+            }}
+            cx="160.5"
+            cy="114.5"
+            r="101.5"
+            fill="#262626"
+        />
+        <motion.ellipse
+            variants={{
+              hover: {
+                scaleY: 2.25,
+                y: -25,
+              },
+            }}
+            transition={{
+              duration: 1,
+              ease: "backInOut",
+              delay: 0.2,
+            }}
+            cx="160.5"
+            cy="265.5"
+            rx="101.5"
+            ry="43.5"
+            fill="#262626"
+        />
+      </motion.svg>
+  )
+}
+
 const FeaturedItem = ({image, title, time, summary, link, platform}) => {
   return (
       <motion.li
@@ -16,10 +75,10 @@ const FeaturedItem = ({image, title, time, summary, link, platform}) => {
               scale: 1.05,
             },
           }}
-          className="relative col-span-1 w-full p-4 bg-[#ebdfd5] border border-solid border-black rounded-2xl">
+          className="relative h-[30rem] w-[30rem] shrink-0 overflow-hidden col-span-1 p-4 bg-[#ebdfd5] border border-solid border-black rounded-2xl">
         <div className="relative z-10 text-accent text-left">
           <motion.span
-              initial={{scale:0.85}}
+              initial={{scale: 0.85}}
               variants={{
                 hover: {
                   scale: 1,
@@ -42,6 +101,7 @@ const FeaturedItem = ({image, title, time, summary, link, platform}) => {
               transition-colors hover:bg-white/30 hover:text-neutral-800">
           View {platform}
         </button>
+        <Background/>
       </motion.li>
   )
 }
