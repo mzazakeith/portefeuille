eba9eed4daf00fec859593219a4d15957fe9dc85
mzazakeith <mzazakeith@gmail.com>
2024-03-11 12:25:26 +0300

    cards styling done

--- pages/more/index.js
+++ pages/more/index.js
@@ -38,7 +38,7 @@ const Background = ( ) => {
             cx="160.5"
             cy="114.5"
             r="101.5"
-            fill="#262626"
+            fill="#6589b580"
         />
         <motion.ellipse
             variants={{
@@ -56,13 +56,13 @@ const Background = ( ) => {
             cy="265.5"
             rx="101.5"
             ry="43.5"
-            fill="#262626"
+            fill="#6589b580"
         />
       </motion.svg>
   )
 }
 
-const FeaturedItem = ({image, title, time, summary, link, platform}) => {
+const FeaturedItem = ({ titleLineOne,titleLineTwo, type, summary, link, platform}) => {
   return (
       <motion.li
           whileHover="hover"
@@ -75,28 +75,31 @@ const FeaturedItem = ({image, title, time, summary, link, platform}) => {
               scale: 1.05,
             },
           }}
-          className="relative h-[30rem] w-[30rem] shrink-0 overflow-hidden col-span-1 p-4 bg-[#ebdfd5] border border-solid border-black rounded-2xl">
-        <div className="relative z-10 text-accent text-left">
-          <motion.span
-              initial={{scale: 0.85}}
-              variants={{
-                hover: {
-                  scale: 1,
-                },
-              }}
-              transition={{
-                duration: 1,
-                ease: "backInOut",
-              }}
-              className="my-2 block origin-top-left font-mono text-6xl font-black leading-[1.2]">
-            $299/
-            <br/>
-            Month
-          </motion.span>
-          <p className="text-sm mb-2">{summary}</p>
-        </div>
-        <button
-            className="absolute bottom-4 left-4 right-4 z-20 rounded border-2 border-accent bg-accent py-2
+          className="relative h-[30rem] w-[30rem] shrink-0 overflow-hidden col-span-1 p-4 bg-[#ebdfd5] border border-solid border-[#6589b580] rounded-2xl shadow-2xl">
+          <div className="relative z-10 text-accent text-left">
+            <span className="mb-3 block w-fit rounded-full bg-accent/30 px-3 py-2 text-sm font-light text-accent">
+          {type}
+        </span>
+              <motion.span
+                  initial={{scale: 0.85}}
+                  variants={{
+                      hover: {
+                          scale: 1,
+                      },
+                  }}
+                  transition={{
+                      duration: 1,
+                      ease: "backInOut",
+                  }}
+                  className="my-2 block origin-top-left font-mono text-6xl font-black leading-[1.2]">
+                  {titleLineOne}
+                  <br/>
+                  {titleLineTwo}
+              </motion.span>
+              <p className="text-sm mb-2">{summary}</p>
+          </div>
+          <button
+              className="absolute bottom-4 left-4 right-4 z-20 rounded border-2 border-accent bg-accent py-2
              text-center font-mono font-black uppercase text-neutral-800 backdrop-blur
               transition-colors hover:bg-white/30 hover:text-neutral-800">
           View {platform}
@@ -106,16 +109,6 @@ const FeaturedItem = ({image, title, time, summary, link, platform}) => {
   )
 }
 
-// <Link href={link} target="_blank"
-//       className="w-full cursor-pointer overflow-hidden rounded-lg"
-// >
-//   <Image width={300} height={500} src={image} alt={title} className="w-full h-auto"/>
-// </Link>
-// <Link href={link} target="_blank">
-//   <h2 className="capitalize text-2xl font-bold my-2">{title}</h2>
-//
-//   <span className="text-accent font-semibold">{time}</span>
-// </Link>
 
 const More = () => {
   return (
@@ -129,21 +122,23 @@ const More = () => {
 
         <ul className="grid grid-cols-2 gap-16">
           <FeaturedItem
-              title="NextJS Portfolio Website"
+              titleLineOne="Africki"
+              titleLineTwo="Kennels"
               link="https://mzazakeith.github.io/portefeuille/"
-              time="10 min"
+              type="Hobby/Business"
               platform="Instagram"
-              image="https://raw.githubusercontent.com/codebucks27/Next.js-Developer-Portfolio-Starter-Code/main/public/images/articles/smooth%20scrolling%20in%20reactjs.png"
               summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth
-                page transitions, cool background effects, unique design and it is mobile responsive."/>
+                page transitions, cool background effects, unique design and it is mobile responsive. page transitions, cool background effects, unique design and it is mobile responsive.page transitions,
+                cool background effects, unique design and it is mobile responsive."/>
           <FeaturedItem
-              title="NextJS Portfolio Website"
+              titleLineOne="Africki"
+              titleLineTwo="Kennels"
               link="https://mzazakeith.github.io/portefeuille/"
-              time="10 min"
+              type="Blog"
               platform="Hashnode"
-              image="https://raw.githubusercontent.com/codebucks27/Next.js-Developer-Portfolio-Starter-Code/main/public/images/articles/create%20modal%20component%20in%20react%20using%20react%20portals.png"
               summary="A professional portfolio website using React JS, Framer-motion, and Styled-components. It has smooth
-                page transitions, cool background effects, unique design and it is mobile responsive."/>
+                page transitions, cool background effects, unique design and it is mobile responsive.
+                page transitions, cool background effects, unique design and it is mobile responsive.page transitions, cool background effects, unique design and it is mobile responsive."/>
         </ul>
       </div>
   )
