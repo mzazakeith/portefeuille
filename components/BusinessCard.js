2971a6e2dd76e298658867b2977cb5b2e1f9f39d
mzazakeith <mzazakeith@gmail.com>
2024-03-11 16:23:54 +0300

    styling done for business card component

--- components/BusinessCard.js
+++ components/BusinessCard.js
@@ -1,8 +1,45 @@
+import Image from "next/image";
+import {useRouter} from "next/router";
 
 
 const BusinessCard = () => {
+    const router = useRouter();
     return(
-        <div className="relative min-h-[200px] max-w-[400px] w-full rounded-lg shadow-2xl bg-[#BEB5AF] m-[2rem] overflow-hidden">
+        <div
+            className="relative min-h-[200px] max-w-[400px] w-full rounded-lg shadow-2xl bg-[#BEB5AF] m-[2rem] overflow-hidden">
+            <div className="flex flex-col justify-center m-4 mb-0">
+                <div className="flex flex-row items-start justify-between">
+                    <h1 className="text-2xl">Keith Mzaza</h1>
+                    <div className="bg-[#BEB5AF] rounded-md z-10
+                border border-solid flex flex-col justify-center items-center border-[#A69F99]/50">
+                        <Image src={`${router.basePath}/my-avatar.png`} alt='' width={50} height={50}
+                        />
+                    </div>
+                </div>
+                <div className="flex flex-row items-start">
+                    <p className="text-accent">Fullstack Developer & DevOps Engineer</p>
+                </div>
+            </div>
+
+            <div className="flex flex-col pb-5 pt-2 gap-1">
+                <div className="grid grid-cols-2 mr-8">
+                    <span className="text-black font-bold col-span-1 text-left ml-4">email</span>
+                    <span className="font-light z-10 col-span-1 text-left">mzazakeith@gmail.com</span>
+                </div>
+                <div className="grid grid-cols-2 mr-8">
+                    <span className="text-black font-bold col-span-1 text-left ml-4">linkedIn</span>
+                    <span className="font-light z-10 col-span-1 text-left">k-mzaza</span>
+                </div>
+                <div className="grid grid-cols-2 mr-8">
+                    <span className="text-black font-bold col-span-1 text-left ml-4">github</span>
+                    <span className="font-light z-10 col-span-1 text-left">mzazakeith</span>
+                </div>
+
+                <div className="grid grid-cols-2 mr-8">
+                    <span className="text-black font-bold col-span-1 text-left ml-4">hashnode</span>
+                    <span className="font-light z-10 col-span-1 text-left">mzaza.hashnode.dev</span>
+                </div>
+            </div>
 
             <div className="absolute right-0 bottom-0 z-0">
                 <div className="w-[260px] h-[260px] bg-[#A69F99]/50 rounded-full"></div>
--- components/Transition.js
+++ components/Transition.js
@@ -18,13 +18,13 @@ const transitionVariants = {
 const Transition = () => {
   return (
     <>
-      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#A69F99]'
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-50 bg-[#A69F99]'
                   variants={transitionVariants} initial='initial' animate='animate' exit='exit'
                   transition={{delay:0.2, duration:0.6, ease: 'easeInOut'}}></motion.div>
-        <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#BEB5AF]'
+        <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-40 bg-[#BEB5AF]'
                     variants={transitionVariants} initial='initial' animate='animate' exit='exit'
                     transition={{delay:0.4, duration:0.6, ease: 'easeInOut'}}></motion.div>
-        <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#D5CCC4]'
+        <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#D5CCC4]'
                     variants={transitionVariants} initial='initial' animate='animate' exit='exit'
                     transition={{delay:0.6, duration:0.6, ease: 'easeInOut'}}></motion.div>
     </>
