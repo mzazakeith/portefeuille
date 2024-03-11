d36b6939d6c4e8067d20815bf0c413ccc3457ef2
mzazakeith <mzazakeith@gmail.com>
2024-03-11 20:38:27 +0300

    business card styled

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -10,7 +10,7 @@ const Contact = () => {
         <div className='container mx-auto h-full flex flex-col items-center gap-x-6'>
           <motion.h1
               variants={fadeIn('down', 0.2)} initial='hidden' animate='show' exit='hidden'
-              className='h1 mt-2 xl:mt-auto'>
+              className='h1 mt-2 xl:mt-auto uppercase font-light'>
             Get In Touch
           </motion.h1>
             <BusinessCard/>
