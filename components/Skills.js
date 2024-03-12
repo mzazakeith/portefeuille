3973375210d05b27c521d4bd3a0b654d227067fd
mzazakeith <mzazakeith@gmail.com>
2024-03-12 09:48:23 +0300

    skills section

--- components/Skills.js
+++ components/Skills.js
@@ -70,14 +70,14 @@ const LinkBox = ({Icon, href}) =>{
         >
 
 
-            <Icon className="text-xl sm:text-3xl md:text-4xl text-neutral-900"/>
+            <Icon className="text-xl sm:text-3xl md:text-4xl text-neutral-900 dark:text-shade"/>
 
             {/*overlay div*/}
             <div
                 ref={scope}
                 style={{clipPath:BOTTOM_RIGHT_CLIP}}
-                className="absolute inset-0 grid place-content-center bg-neutral-900">
-                <Icon className="text-xl sm:text-3xl md:text-4xl text-primary"/>
+                className="absolute inset-0 grid place-content-center bg-neutral-900 dark:bg-shade">
+                <Icon className="text-xl sm:text-3xl md:text-4xl text-primary dark:text-neutral-900"/>
             </div>
         </a>
     );
@@ -85,21 +85,21 @@ const LinkBox = ({Icon, href}) =>{
 
 const ClipPathLinks = () => {
     return(
-        <div className="border border-neutral-900 divide-y divide-neutral-900">
-            <div className="grid grid-cols-3 divide-x divide-neutral-900">
+        <div className="border border-neutral-900 divide-y divide-neutral-900 dark:divide-shade dark:border-shade">
+            <div className="grid grid-cols-3 divide-x divide-neutral-900 dark:divide-shade">
                 <LinkBox Icon={TbBrandReact} href="" />
                 <LinkBox Icon={SiTailwindcss} href="" />
                 <LinkBox Icon={TbBrandNextjs} href="" />
             </div>
 
-            <div className="grid grid-cols-4 divide-x divide-neutral-900">
+            <div className="grid grid-cols-4 divide-x divide-neutral-900 dark:divide-shade">
                 <LinkBox Icon={TbBrandTerraform} href="" />
                 <LinkBox Icon={SiAmazonaws} href="" />
                 <LinkBox Icon={TbBrandAnsible} href="" />
                 <LinkBox Icon={SiKubernetes} href="" />
             </div>
 
-            <div className="grid grid-cols-3 divide-x divide-neutral-900">
+            <div className="grid grid-cols-3 divide-x divide-neutral-900 dark:divide-shade">
                 <LinkBox Icon={TbBrandPython} href="" />
                 <LinkBox Icon={FaNode} href="" />
                 <LinkBox Icon={BiLogoSpringBoot} href="" />
@@ -112,7 +112,7 @@ const ClipPathLinks = () => {
 const Skills = () => {
     return(
         <div className="mt-36 w-full">
-            <h2 className="font-light uppercase h2 mb-20 w-full text-center">Skills</h2>
+            <h2 className="font-light uppercase h2 mb-20 w-full text-center dark:text-shade">Skills</h2>
             <div className="mx-auto max-w-7xl shadow-2xl">
                 <ClipPathLinks/>
             </div>
