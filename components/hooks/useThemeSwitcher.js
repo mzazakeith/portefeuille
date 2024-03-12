c0ff09b0581a496e095ce2e5a2172313a5c45884
mzazakeith <mzazakeith@gmail.com>
2024-03-12 14:24:11 +0300

    removing OS mode preference. users will switch manually

--- components/hooks/useThemeSwitcher.js
+++ components/hooks/useThemeSwitcher.js
@@ -1,40 +1,21 @@
 import {useEffect, useState} from "react";
 
-const useThemeSwitcher = () =>{
-    const preferDarkQuery = ("prefer-color-scheme: dark");
-    const [mode, setMode] = useState("light");
+const useThemeSwitcher = () => {
+    const [mode, setMode] = useState("");
+
     useEffect(() => {
-       const mediaQuery = window.matchMedia(preferDarkQuery);
-       const userPreference = window.localStorage.getItem("theme");
-       const handleChange = () =>{
-           if(userPreference){
-               let check = userPreference === "dark" ? "dark" : "light"
-               setMode(check);
-               if(check === "dark"){
-                   document.documentElement.classList.add("dark")
-               }else{
-                   document.documentElement.classList.remove("dark")
-               }
-           }else{
-               setMode("light");
-               document.documentElement.classList.remove("dark")
-           }
-       }
-       mediaQuery.addEventListener("change", handleChange)
-        return () => mediaQuery.removeEventListener("change", handleChange)
+        const userPreference = window.localStorage.getItem("theme") || "light";
+        setMode(userPreference);
+        document.documentElement.classList.toggle("dark", userPreference === "dark");
     }, []);
 
-    useEffect(() => {
-        if(mode === "dark"){
-            window.localStorage.setItem("theme","dark");
-            document.documentElement.classList.add("dark")
-        }else{
-            window.localStorage.setItem("theme","light");
-            document.documentElement.classList.remove("dark")
-        }
-    }, [mode]);
+    const setThemeMode = (selectedMode) => {
+        setMode(selectedMode);
+        window.localStorage.setItem("theme", selectedMode);
+        document.documentElement.classList.toggle("dark", selectedMode === "dark");
+    };
 
-    return [mode, setMode]
-}
+    return [mode, setThemeMode];
+};
 
 export default useThemeSwitcher;
